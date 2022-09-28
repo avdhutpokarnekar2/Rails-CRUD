50bb1b6e30992bed596977e89657649811d3fde0
Avdhut Pokarnekar <apokarnekar@gmail.com>
2022-09-28 18:49:59 +0530

    first commit

---
+++ db/seeds.rb
@@ -0,0 +1,15 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+   Article.create ([{
+ 	title: 'avdhut@ed.com',	body: 'Aap@12345' 
+  }])
+
+
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
